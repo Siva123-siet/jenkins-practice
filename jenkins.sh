1d4fe67dabfc82ec893719a8957dd614a5f420e9
Siva <siva.danthuluri@gmail.com>
2026-06-16 15:18:31 +0530

    jenkins

---
+++ jenkins.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo curl -o /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/rpm-stable/jenkins.repo
+sudo yum upgrade
+# Add required dependencies for the jenkins package
+sudo yum install fontconfig java-21-openjdk
+sudo yum install jenkins
+sudo systemctl daemon-reload
+
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
