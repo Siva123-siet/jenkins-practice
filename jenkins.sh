f90cb86f1c9fe8736b4604480dcd84c286934027
Siva <siva.danthuluri@gmail.com>
2026-06-16 16:15:46 +0530

    jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -3,6 +3,7 @@
 sudo yum upgrade -y
 sudo curl -o /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/rpm-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/rpm-stable/jenkins.io.key
 # Add required dependencies for the jenkins package
 sudo yum install fontconfig java-21-openjdk -y
 sudo yum install jenkins -y
